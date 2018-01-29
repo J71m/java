032f694ad6738ccb3911c0d636c2d77fb559cab9
Tim Jaanson <jaantim@greeny.cs.tlu.ee>
2018-01-29 09:18:53 +0200

    ayy

---
+++ hi.java
@@ -0,0 +1,15 @@
+public class hi{
+	public static void main(String[] arg) {
+		System.out.println("Hi");
+		int temp=-10;
+		if(temp<=-28){
+			System.out.println("külmapüha");
+		}
+		if(temp>=15){
+			System.out.println("T-särk soovituslik");
+		}else{
+			System.out.println("kooli");
+		}
+	}
+	
+}
